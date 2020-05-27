8d02e048488dd4b64e0bab28c22a936d5f73630f
mtkramer <kramerm1@mail.usf.edu>
2020-05-27 15:58:56 -0400

    bonus 3 and 4 complete

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -8,11 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class EquipmentComponent implements OnInit {
 
   equipment: string[] = ['Habitat dome', 'Drones', 'Food containers', 'Oxygen tanks'];
+  editing: string;
 
   constructor() { }
 
   addItem(item: string): void {
     this.equipment.push(item);
+  }
+
+  removeItem(item): void {
+    this.equipment.splice(this.equipment.indexOf(item), 1);
+  }
+
+  editItem(item): void {
+    let index = this.equipment.lastIndexOf(this.editing);
+    this.equipment[index] = item;
   }
 
   ngOnInit() {
