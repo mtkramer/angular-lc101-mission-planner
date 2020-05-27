8af6863cecd693baf8afa2a30f3392bba63429f9
mtkramer <kramerm1@mail.usf.edu>
2020-05-27 16:25:21 -0400

    bonus 6 complete

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -8,11 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class ExperimentsComponent implements OnInit {
 
   experiments = ['Mars soil sample', 'Plant growth in habitat', 'Human bone density'];
+  editing: string;
 
   constructor() { }
 
   addExperiment(experiment): void {
     this.experiments.push(experiment);
+  }
+
+  editExperiment(experiment): void {
+    let index = this.experiments.indexOf(this.editing);
+    this.experiments[index] = experiment;
+    this.editing = '';
   }
 
   ngOnInit() {
